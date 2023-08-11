73ef6c9850ac14e4e90e1f3c48d0d847107fa3ad
progLearn2635_home_pc <proglearn2635@gmail.com>
2023-08-11 19:18:05 +0000

    init

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import awsExports from './src/aws-exports';
+Amplify.configure(awsExports);
 
 export default function App() {
   return (
